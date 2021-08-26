84a931c493ad6d95cbf80a1890297b769217e011
Amanda <amandaichigo123@gmail.com>
2021-08-26 00:06:24 -0300

    Routes
    Criação de rotas e da estrutura da aplicação

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express"); 
+const app = express(); 
+const routes = require("./routes");
+
+const port=3000; 
+const address="localhost";
+
+//Criando um servidor simples com o Node.js e o Express 
+
+app.use('/',routes);
+
+const server = app.listen(port,address,()=>{ 
+    let host = server.address().address; 
+    let port = server.address().port; 
+    console.log(`Servidor executando no endereço ${host} e porta ${port}`); 
+});
+//Para parar o servidor, use ctrl + c
